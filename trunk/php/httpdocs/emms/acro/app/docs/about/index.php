1f17330fbe2e33ca270c79db299cbc3a63c32aaf
zicare <zicare@62c450fc-914c-0410-bbc9-ad1a315dd7f8>
2009-12-30 15:35:20 +0000

    EMMS 2.5 intial load

---
+++ trunk/php/httpdocs/emms/acro/app/docs/about/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+  <head>
+    <title>About EMMS</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-3">
+    <link href='../../themes/blue/style.about.css' rel='stylesheet' type='text/css'>
+  </head>
+  <body background="../../themes/blue/logo-watermark.png">
+    <h2>About EMMS</h2>
+    <p>EMMS is a web based microcredit management information system.
+       It provides the key functionality required by MFIs, including client and loan portfolio management,
+       loan repayment tracking, reporting and tools for social impact.
+       <br /><br />
+       To learn more about the EMMS project please visit the project home page at www.e-mms.org
+       <br /><br />
+       EMMS is sponsored by Esperanza International, a non-profit christian development organization devoted
+       to free children and their families from poverty through initiatives that generate income, education
+       and health, restoring self-worth and dignity to those who have lost hope.
+       <br /><br />
+       To learn more about Esperanza International please visit us on the web at www.esperanza.org
+       <br /><br /><br /><br />
+       <img src='girl.gif' alt=''>
+       <div class='version'><?php include "../../../version"; ?></div>
+  </body>
+</html>
