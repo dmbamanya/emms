326056474b363d3c72c2086776258e280f32fb6e
zicare <zicare@62c450fc-914c-0410-bbc9-ad1a315dd7f8>
2010-09-09 20:58:42 +0000

    Update 2.53 - Added support for XML-RPC, new columns added to several reports, automatic sponsor selection at LN.SCR.doCheckDisbursement. This update requires the XML_RPC2 PEAR package.

---
+++ trunk/emms2.5/php/httpdocs/emms/acro/app/index.rpc.php
@@ -0,0 +1,27 @@
+<?php
+error_reporting(E_ERROR);
+require_once 'class/webpage.php';
+require_once 'class/sql.php';
+require_once 'PEAR.php';
+require_once 'Auth.php';
+
+WEBPAGE::START();
+WEBPAGE::$lang = $_GET['lang'] ? $_GET['lang'] : WEBPAGE::$conf['app']['default_language'];
+WEBPAGE::load_gt();
+WEBPAGE::load_fn();
+
+// check user auth. status
+$auth = new Auth('DB', WEBPAGE::$auth_options, 'checkAccess');
+$auth->start();
+
+// to be here you need to provide a trusted scr_name... see checkAccess function
+//WEBPAGE::LOAD_SESSION();
+
+require './includes/index.rpc.inc';
+
+function checkAccess()
+{ 
+  // (WEBPAGE::$fn['mask'][$_GET['scr_name']] == '255') ? '' : exit;
+}
+
+?>
